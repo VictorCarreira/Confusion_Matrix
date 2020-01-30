43db0196f8cd1a962f58afef00a49c9daa6e1b3f
VictorCarreira <carreiravr@gmail.com>
2020-01-30 15:43:19 -0300

    Adiciona o dado sintético e vai até o drop

--- CM.py
+++ CM.py
@@ -1,5 +1,6 @@
-#This program implement the confusion matrix 
+#This program implements a confusion matrix 
 #Author: Victor Carreira
+
 ###################################### THEORY ################################################
 #    In the field of machine learning and specifically the problem of statistical classification, a confusion matrix, also known as an error matrix,[6] is a specific table layout that allows visualization of the performance of an algorithm, typically a supervised learning one (in unsupervised learning it is usually called a matching matrix).      
 #    Each row of the matrix represents the instances in a predicted class while each column represents the instances in an actual class (or vice versa).[2] The name stems from the fact that it makes it easy to see if the system is confusing two classes (i.e. commonly mislabeling one as another).
@@ -60,11 +61,31 @@ print(tn, fp, fn, tp)
 # Plotting confusion matrix
 
 
-columns = ['class %s' %(i) for i in list(ascii_uppercase)[0:len(np.unique(y_true1))]]
+columns = ['class %s' %(i) for i in list(ascii_uppercase)[0:len(np.unique(y_true2))]]
 
-confm = confusion_matrix(y_true1, y_pred1)
-df_cm = DataFrame(C1, index=columns, columns=columns)
+confm = confusion_matrix(y_true2, y_pred2)
+df_cm = DataFrame(confm, index=columns, columns=columns)
 
 ax = sn.heatmap(df_cm, cmap='Oranges', annot=True)
 
 plt.show()
+
+
+# Application in a synthetic scennario:
+# There are two input vectors. One is the predicted code vectors provided by the model os parameters from neural network. Second one is the real set of code vectors. 
+
+
+#OBS: The data used in this secton is related to my PhD thesis. Therefore is sigilous and omited. In a near future it will be release. 
+
+
+data = pd.read_table("dados_class_c1r5%.txt", delim_whitespace=True, names=('Counter','Class' ,'Predicted(ANN)', 'Delimiter','Calculated(true)','Error'))
+df=pd.DataFrame(data, columns=['Counter','Class', 'Predicted(ANN)','Delimiter','Calculated(true)','Error'])
+df=df.drop('Class',axis=1) #rips of counter Class String colunm
+df=df.drop('Delimiter',axis=1)#rips of Delimiter 
+df=df.drop('Error', axis=1)# Error
+df=df.drop('Counter',axis=1)# Counter
+print(df)
+
+
+
+
